6afc86f5060814ed84e8ef1489c087b473dbe198
Logie <marlog@ceh.ac.uk>
2021-04-20 10:48:32 +0100

    Adding new responses

--- responses.R
+++ responses.R
@@ -31,6 +31,7 @@ Wallingford,
 Oxfordshire OX10 8BB',
 `Hoverfly` = 'Thank you so much for your e-mail. We receive lots of reports of native species that look like Asian hornets and I am pleased to say that the photograph you have sent is not of an Asian Hornet. Your photograph shows a hoverfly, many of which are extremely good wasp and bee mimics. I have pasted a couple of links for further information about hoverflies:
   
+https://www.wildlifetrusts.org/wildlife-explorer/invertebrates/flies/drone-fly
 https://www.buglife.org.uk/bugs-and-habitats/hornet-hoverfly 
 https://www.naturespot.org.uk/species/common-drone-fly
 http://www.microscopy-uk.org.uk/mag/artmay07/cd-hoverflies.html 
@@ -97,5 +98,193 @@ UK Centre for Ecology & Hydrology
 Benson Lane,
 Crowmarsh Gifford
 Wallingford
-Oxfordshire OX10 8BB')
+Oxfordshire OX10 8BB',
+`Birch Sawfly` = 'Thank you so much for your record.  We receive lots of reports of native species that look like Asian hornets and I am pleased to say that the photograph you have sent is not of an Asian Hornet. Your photograph looks to show a type of sawfly called the Birch Sawfly (Cimbex femoratus). Like hornets it is also a Hymenopteran but the larvae feed on birch leaves unlike wasps. It is completely harmless and a fantastic record, because although this species is fairly widespread and common throughout the UK, the adults are not seen that regularly. The larvae can often be easily found on birch trees and are equally impressive. More information can be found at the following sites for example:
+ 
+https://en.wikipedia.org/wiki/Cimbex_femoratus
 
+http://eol.org/pages/3763965/overview
+ 
+http://www.naturespot.org.uk/species/birch-sawfly
+
+Thank you again for your report – such reports are extremely useful for non-native species surveillance.  Please do report future sightings of concern by using the on-line form.
+
+If you have a smart phone or tablet / iPad, you can also use the Asian Hornet Watch app, which is available free for Android and Apple devices:
+https://play.google.com/store/apps/details?id=uk.ac.ceh.hornets  
+https://itunes.apple.com/gb/app/asian-hornet-watch/id1161238813 
+ 
+For detailed information and help on identification of the Asian Hornet please also see the following factsheets:
+
+https://secure.fera.defra.gov.uk/beebase/downloadDocument.cfm?id=698
+
+http://www.bwars.com/sites/www.bwars.com/files/info_sheets/Velutina_20160921_0.pdf
+
+For an overview of the Asian Hornet and its status in GB please see:
+
+https://secure.fera.defra.gov.uk/nonnativespecies/factsheet/factsheet.cfm?speciesId=3826
+ 
+Very best wishes, 
+ 
+GB Non-Native Species Information Portal (GB-NNSIP)
+UK Centre for Ecology & Hydrology
+Benson Lane,
+Crowmarsh Gifford
+Wallingford
+Oxfordshire OX10 8BB',
+`Social Wasp` = 'Thank you so much for your report. We receive lots of reports of native species that look like Asian hornets and I am pleased to say that the photograph you have sent is not of an Asian Hornet. Your photograph shows a native species of social wasp (Vespula/Dolichovespula spp). There are several similar species and from the attached photograph it is not possible to accurately identify which species it is. More information on native bee and wasp species in the UK can be found on the Bees, Wasps and Ants Recording Society’s (BWARS) excellent website:
+ 
+http://www.bwars.com/
+
+Thank you again for your report – such reports are extremely useful for non-native species surveillance.  Please do report any future sightings of concern by either e-mailing this address or using the on-line form:
+ 
+http://www.brc.ac.uk/risc/alert.php?species=asian_hornet
+ 
+ 
+For detailed information and help on identification of the Asian Hornet please also see the following factsheets:
+
+http://www.bwars.com/sites/www.bwars.com/files/info_sheets/Velutina_20160921_0.pdf
+ 
+https://secure.fera.defra.gov.uk/beebase/downloadDocument.cfm?id=698
+
+
+For an overview of the Asian Hornet and its status in GB please see:
+ 
+https://secure.fera.defra.gov.uk/nonnativespecies/factsheet/factsheet.cfm?speciesId=3826
+ 
+ 
+ 
+Very best wishes, 
+ 
+GB Non-Native Species Information Portal (GB-NNSIP)
+UK Centre for Ecology & Hydrology
+Benson Lane,
+Crowmarsh Gifford
+Wallingford
+Oxfordshire OX10 8BB',
+`Horsefly Tabanus` = 'Thank you so much for your report.  We receive lots of reports of native species that look like Asian hornets and I am pleased to say that the photograph you have sent is not of an Asian Hornet. Your photograph shows a species horse fly from the genus Tabanus. I have added a couple of links for further information about horse flies:
+
+http://www.naturespot.org.uk/taxonomy/term/19440
+http://www.brc.ac.uk/soldierflies-and-allies/resources
+http://influentialpoints.com/Gallery/Horseflies_Clegs_and_Deerflies.htm
+
+
+Thank you again for your report – such reports are extremely useful for non-native species surveillance.  Please do report any future sightings of concern by either e-mailing this address or using the on-line form:
+http://www.brc.ac.uk/risc/alert.php?species=asian_hornet or through the free Asian Hornet Watch recording app for Android and Apple devices: https://play.google.com/store/apps/details?id=uk.ac.ceh.hornets or https://itunes.apple.com/gb/app/asian-hornet-watch/id1161238813. This helps us by adding your record directly into our species database where we will endeavour to provide feedback on the identity of the insect you have reported as soon as possible, and can share information with others involved with surveillance for this species.
+
+
+
+For an overview of the Asian Hornet and its status in the GB please see:
+
+https://secure.fera.defra.gov.uk/nonnativespecies/factsheet/factsheet.cfm?speciesId=3826
+
+
+For detailed information and help on identification of the Asian Hornet please also see the following factsheets:
+
+https://secure.fera.defra.gov.uk/beebase/downloadDocument.cfm?id=698
+
+http://www.bwars.com/sites/www.bwars.com/files/info_sheets/Velutina_20160921_0.pdf
+
+
+Very best wishes,
+
+GB Non-Native Species Information Portal (GB-NNSIP) 
+UK Centre for Ecology & Hydrology 
+Benson Lane, 
+Crowmarsh Gifford 
+Wallingford 
+Oxfordshire
+OX10 8BB',
+`Ichneumonid` = 'Thank you so much for your report.  We receive lots of reports of native species that look like Asian hornets and I am pleased to say that the photograph you have sent is not of an Asian Hornet. Your photograph shows a species of ichneumonid, a type of parasitoid wasp. This is one of the largest groups of insects and there are many very similar species, many of which require dissection to ascertain which species they are and are rarely identifiable to species level from photographs. Although they appear quite different to the insects we commonly refer to as ‘wasps’ (e.g. Common Wasp, Vespula vulgaris), many mimic the yellow and black coloration for defense, and they are also Hymenopterans. More information can be found at the following sites:
+ 
+https://en.wikipedia.org/wiki/Ichneumonidae
+ 
+http://www.nhm.ac.uk/content/dam/nhmwww/take-part/identify-nature/british-ichneumonid-wasps-id-guide.pdf
+ 
+http://www.brc.ac.uk/sites/www.brc.ac.uk/files/pictures/resources/ichneumonidae-subfamily-key.pdf
+
+ 
+Thank you again for your report – such reports are extremely useful for non-native species surveillance.  If you have a smart phone or tablet / iPad, please report any future sightings of concern by either using the Asian Hornet Watch app, which is available free for Android and Apple devices:
+ 
+https://play.google.com/store/apps/details?id=uk.ac.ceh.hornets 
+https://itunes.apple.com/gb/app/asian-hornet-watch/id1161238813
+ 
+or otherwise by using the on-line form:
+http://www.brc.ac.uk/risc/alert.php?species=asian_hornet
+ 
+or e-mailing this address.
+ 
+For detailed information and help on identification of the Asian Hornet please also see the following factsheets:
+ 
+https://secure.fera.defra.gov.uk/beebase/downloadDocument.cfm?id=698
+ 
+http://www.bwars.com/sites/www.bwars.com/files/info_sheets/Velutina_20160921_0.pdf
+ 
+ 
+For an overview of the Asian Hornet and its status in the GB please see:
+ 
+https://secure.fera.defra.gov.uk/nonnativespecies/factsheet/factsheet.cfm?speciesId=3826
+ 
+ 
+Very best wishes, 
+
+ 
+GB Non-Native Species Information Portal (GB-NNSIP),
+UK Centre for Ecology & Hydrology,
+Benson Lane,
+Crowmarsh Gifford,
+Wallingford,
+Oxfordshire OX10 8BB',
+`Missing Locality` = 'Thank you so much for your report.  Please could you respond with details of  the location of your sighting so the record can be added to the database, a postcode or village / town name would be fine. 
+
+Please also take a look at the following identification sheets and do get back to us if you are still concerned that it may have been an Asian hornet: Identification sheet and https://secure.fera.defra.gov.uk/beebase/downloadDocument.cfm?id=698. If you wish for assistance you can contact your local beekeeping association by using the British Beekeepers Association Asian hornet action team map. https://www.bbka.org.uk/asian-hornet-action-team-map  Zoom into your locality to find a coordinators email address and telephone number and they will arrange contact with a local beekeeper to help with identifying and reporting.
+
+Reports such as yours are essential in maintaining early detection if this species does arrive in the UK. We receive many reports throughout the year, most of which are of the native European Hornet, Vespa crabro, which is slightly larger than the Asian Hornet, but with a lot more yellow on the abdomen. Other reports include other wasp species, especially the larger queens, bees and a range of wasp and bee mimic hoverflies. The Bees, Wasps & Ants Recording Society (BWARS) have an excellent website on which more information on native species can be found. For example, information on the European Hornet can be found at: http://www.bwars.com/sites/www.bwars.com/files/info_sheets/Vespa-crabro-info-sheet.pdf 
+
+Thank you again for this report - such reports are extremely useful for non-native species surveillance.  Please do report future sightings of concern by using the on-line form: http://www.brc.ac.uk/risc/alert.php?species=asian_hornet
+
+If you send a photograph we will endeavour to identify your sighting and confirm what it is. This is also useful for recording the distribution of native species.
+
+For an overview of the Asian Hornet and its status in GB please see: https://secure.fera.defra.gov.uk/nonnativespecies/factsheet/factsheet.cfm?speciesId=3826 
+For further information and help on identification of the Asian Hornet please also see the following factsheet: 
+http://www.bwars.com/sites/www.bwars.com/files/info_sheets/Velutina_20160921_0.pdf 
+
+Very best wishes, 
+ 
+ 
+GB Non-Native Species Information Portal (GB-NNSIP)
+UK Centre for Ecology & Hydrology
+Benson Lane,
+Crowmarsh Gifford
+Wallingford
+Oxfordshire OX10 8BB
+
+Address for specimens - only to be sent where a picture cannot be obtained (please include ID number as a reference, found in the subject line of the email):
+NBULab, 
+02GO6, 
+York Biotech, 
+Sand Hutton, 
+York, 
+YO41 1LZ',
+`American record` = 'Many thanks for sending in a record of your suspected sighting. This account focuses on the recording of Asian hornets in the U.K., please could I check that your record was submitted with the correct co-ordinates as the location is showing as being in America\\?
+
+If your record is indeed from America please see the following guidance: 
+
+Reporting a suspected sighting of Asian giant hornet (Vespa mandarinia) from Washington state:
+There is a dedicated online form here: 
+maps.arcgis.com/apps/GeoForm/index.html?appid=e3720c303c414210967920b07bad13f5
+
+Reporting a suspected sighting of Asian giant hornet (Vespa mandarinia) from British Columbia:
+https://bcinvasives.ca/report
+
+If the suspected sighting location is from another state please contact your state or province\'s agriculture department to report the pest.  
+
+For more general species records could I please suggest you submit your record to iNaturalist https://www.inaturalist.org/, there is also BugGuide which has active forums and ID help: https://bugguide.net/forum
+
+Very best wishes, 
+
+GB Non-Native Species Information Portal (GB-NNSIP),
+UK Centre for Ecology & Hydrology,
+Benson Lane,
+Crowmarsh Gifford,
+Wallingford,
+Oxfordshire OX10 8BB')
